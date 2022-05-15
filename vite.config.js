581985a545793aebeea3636085d6d35af87bc684
Kulebaev Konstantin <ku.kulebaev@interfax.ru>
2022-05-15 18:21:08 +0300

    correct additionalData prop

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `
-              @import "./src/assets/styles/style.scss";
-            `,
+        additionalData: '@import "./src/assets/styles/style.scss";',
       },
     },
   },
