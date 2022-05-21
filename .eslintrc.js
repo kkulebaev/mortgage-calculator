66408c513cf3954c584967a9e22c859cc1434045
Kulebaev Konstantin <ku.kulebaev@interfax.ru>
2022-05-21 12:40:24 +0300

    add extends 'prettier' and remove custom rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,4 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended'],
-  rules: {
-    'vue/max-attributes-per-line': 0,
-    'vue/html-self-closing': 0,
-    'vue/singleline-html-element-content-newline': 0,
-  },
+  extends: ['plugin:vue/vue3-recommended', 'prettier'],
 }
