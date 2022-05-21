d101decf82c47b0785e43ea1b6c3b465d54f9487
Kulebaev Konstantin <ku.kulebaev@interfax.ru>
2022-05-21 12:24:15 +0300

    add custon rule "vue/html-self-closing": 0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
   root: true,
   extends: ['plugin:vue/vue3-recommended'],
-  rules: { 'vue/max-attributes-per-line': 0 },
+  rules: {
+    'vue/max-attributes-per-line': 0,
+    'vue/html-self-closing': 0,
 }
