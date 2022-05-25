bc723d1b225dbf5ed6cee1e74aab6079418097eb
Kulebaev Konstantin <ku.kulebaev@interfax.ru>
2022-05-25 15:06:01 +0300

    Add useData and Pagination of Table

---
+++ src/composables/useData.js
@@ -0,0 +1,18 @@
+import { useStorage } from '@vueuse/core'
+import {
+  TYPE_MORTGAGE_OPTIONS,
+  PERIOD_OPTIONS,
+} from '/src/components/the-form/form-helpers.js'
+
+export function useData() {
+  const inputValues = useStorage('input-values', {
+    mortgageAmount: 30000,
+    initialPayment: 10000,
+    mortgageTerm: 3,
+    mortgagePeriod: PERIOD_OPTIONS[0].value,
+    mortgageRate: 12,
+    paymentType: TYPE_MORTGAGE_OPTIONS[0].value,
+  })
+
+  return { inputValues }
+}
