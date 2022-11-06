bda89f8d96fc8435e5b93da92dc6efb4185fc15e
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-06 15:26:58 +0300

    index.js to compasables

--- src/composables/useData.js
+++ src/composables/useData.js
@@ -4,7 +4,7 @@ import {
   PERIOD_OPTIONS,
 } from '@/components/the-form/form-helpers.js'
 
-export function useData() {
+export default function useData() {
   const inputValues = useStorage('input-values', {
     mortgageAmount: 30000,
     initialPayment: 10000,
