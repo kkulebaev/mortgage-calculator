cafe05ab059a9caccb02a4ca0c2a885076d934bd
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-06 14:47:15 +0300

    add vue/custom-event-name-casing

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,6 @@ module.exports = {
     'vue/component-options-name-casing': ['error', 'PascalCase'],
     'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'vue/component-api-style': ['error', ['composition']],
+    'vue/custom-event-name-casing': ['error', 'camelCase'],
   },
 }
