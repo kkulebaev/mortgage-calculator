e1bb7acb8d879f5e05a407cc3103406fe93f76e2
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-10 23:47:44 +0300

    refactoring types

--- src/helpers/form.ts
+++ src/helpers/form.ts
@@ -1,9 +1,29 @@
+export enum MORTGAGE_TYPE {
+  an = 'an',
+  dif = 'dif',
+}
+
+export enum MORTGAGE_TYPE_LABEL {
+  an = 'Аннуитетный',
+  dif = 'Дифференцированный',
+}
+
+export enum PERIOD {
+  years = 'years',
+  months = 'months',
+}
+
+export enum PERIOD_LABEL {
+  years = 'лет',
+  months = 'мес',
+}
+
 export const TYPE_MORTGAGE_OPTIONS = [
-  { value: 'an', label: 'Аннуитетный' },
-  { value: 'dif', label: 'Дифференцированный' },
+  { value: MORTGAGE_TYPE.an, label: MORTGAGE_TYPE_LABEL.an },
+  { value: MORTGAGE_TYPE.dif, label: MORTGAGE_TYPE_LABEL.dif },
 ] as const
 
 export const PERIOD_OPTIONS = [
-  { value: 'years', label: 'лет' },
-  { value: 'months', label: 'мес' },
+  { value: PERIOD.years, label: PERIOD_LABEL.years },
+  { value: PERIOD.months, label: PERIOD_LABEL.months },
 ] as const
