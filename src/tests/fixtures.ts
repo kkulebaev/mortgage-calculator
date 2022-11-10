c782d4fecede42a9f58130b2e1f6709dbbff75f6
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-10 20:50:11 +0300

    add test for calc-mortgage

---
+++ src/tests/fixtures.ts
@@ -0,0 +1,16 @@
+export const inputValue = {
+  mortgageAmount: 1500000,
+  initialPayment: 500000,
+  mortgageTerm: 10,
+  mortgagePeriod: 'years',
+  mortgageRate: 10,
+  paymentType: 'an',
+}
+
+export const outputValue = {
+  takeValue: 1500000,
+  repayValue: 2085808.8425811431,
+  overpaymentValue: 585808.8425811431,
+  monthlyPayment: 13215.073688176193,
+  totalCost: 2085808.8425811431,
+}
