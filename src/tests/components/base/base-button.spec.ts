9e5d9297f42700ba14a9fdbc8d961b6a778412db
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-10 20:11:32 +0300

    add vitest and @vue/test-utils

---
+++ src/tests/components/base/base-button.spec.ts
@@ -0,0 +1,22 @@
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { it, describe, expect } from 'vitest'
+import { BaseButton } from '@/components'
+
+describe('BaseButton', () => {
+  let wrapper: VueWrapper
+
+  const createComponent = (options = {}) => {
+    wrapper = mount(BaseButton, options)
+  }
+
+  const findButton = () => wrapper.find('button')
+
+  it('should emit event "click" when clicked on it', () => {
+    createComponent()
+    const button = findButton()
+
+    button.trigger('click')
+
+    expect(wrapper.emitted('click')).toBeTruthy()
+  })
+})
