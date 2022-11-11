0fc0a414eb1f507b70ce7b3f36b2249aa66eee6c
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-11 10:44:52 +0300

    add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { createRouter } from 'vue-router'
+import { createPinia } from 'pinia'
 
 // Element Plus
 import 'element-plus/dist/index.css'
@@ -11,10 +12,10 @@ import App from './app.vue'
 
 import routerConfig from './router/router.config'
 
+const pinia = createPinia()
 const app = createApp(App)
-
 const router = createRouter(routerConfig)
 
+app.use(pinia)
 app.use(router)
-
 app.mount('#app')
