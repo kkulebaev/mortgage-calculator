e8629c72b809955fefc1da466f1ab29143672ec9
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-11 13:54:10 +0300

    separate services

--- src/services/calc-mortgage.ts
+++ src/services/calc-mortgage.ts
@@ -1,43 +1,18 @@
-import { PERIOD } from '@/helpers'
+import { MORTGAGE_TYPE } from '@/helpers'
 import type { Input, Output } from '@/store'
+import { calcMortgageAn } from './calc-mortgage-an'
+import { calcMortgageDif } from './calc-mortgage-dif'
 
-export default function calcMortgage({
-  mortgageAmount,
-  initialPayment,
-  mortgageTerm,
-  mortgagePeriod,
-  mortgageRate,
-}: Input): Output {
-  const takeValue = mortgageAmount
+export default function calcMortgage(input: Input): Output {
+  let mortgageOutput
 
-  // estMortgageBody - расчетное значение начального тела кредита.
-  const estMortgageBody = mortgageAmount - initialPayment
-
-  // estMortgageTerm - расчетное значение срока ипотеки выраженное в месяцах. Если пользователь выбрал ввод количества лет, то приводим значение к количеству месяцев
-  const estMortgageTerm =
-    mortgagePeriod === PERIOD.years ? mortgageTerm * 12 : mortgageTerm
-
-  // estMortgageRate - расчетное значение месячной процентной ставки
-  const estMortgageRate = mortgageRate / 100 / 12
-
-  // Ежемесячный платеж по аннуитетному типу равен:
-  const monthlyPayment =
-    estMortgageBody *
-    (estMortgageRate / (1 - Math.pow(1 + estMortgageRate, -estMortgageTerm)))
-
-  // Переплата по ипотеке по аннуитетному типу равна:
-  const overpaymentValue = monthlyPayment * estMortgageTerm - estMortgageBody
-
-  const repayValue = overpaymentValue + mortgageAmount
-
-  const totalCost = repayValue
-
-  return {
-    takeValue,
-    repayValue,
-    overpaymentValue,
-    monthlyPayment,
-    totalCost,
-    paymentTable: [],
+  if (input.paymentType === MORTGAGE_TYPE.an) {
+    mortgageOutput = calcMortgageAn(input)
   }
+
+  if (input.paymentType === MORTGAGE_TYPE.dif) {
+    mortgageOutput = calcMortgageDif(input)
+  }
+
+  return mortgageOutput
 }
