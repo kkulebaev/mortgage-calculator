75dec11564522147e2257ab143d223e0c5ccd085
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-17 21:58:38 +0300

    chore: add sitemap and robots.txt plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
+import Sitemap from 'vite-plugin-sitemap'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), Sitemap()],
 
   resolve: {
     alias: {
