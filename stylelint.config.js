b477ac7f41447e4f55d81a827a6a021efeaa6242
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-17 23:38:24 +0300

    config: stylelint for vue

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
+  extends: [
+    'stylelint-config-standard-scss',
+    'stylelint-config-prettier',
+    'stylelint-config-html/vue',
+  ],
 
   rules: {
     'scss/at-import-partial-extension': 'always',
