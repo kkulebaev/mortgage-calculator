dd2bceccf0db698253b05e0c3027a7502a907081
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-17 19:24:22 +0300

    chore: add social network links

---
+++ src/components/social-networks.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="social-networks">
+    <a
+      v-for="item in SOCIAL_NETWORKS"
+      :key="item.icon"
+      class="social-networks__item"
+      :href="item.link"
+      target="_blank"
+    >
+      <BaseIcon :icon="item.icon" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { BaseIcon } from '@/components'
+import { SOCIAL_NETWORKS } from '@/helpers'
+
+export default defineComponent({
+  name: 'SocialNetworks',
+
+  components: {
+    BaseIcon,
+  },
+
+  setup() {
+    return {
+      SOCIAL_NETWORKS,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.social-networks {
+  display: flex;
+  gap: 1rem;
+}
+
+.social-networks__item {
+  width: 32px;
+}
+</style>
---
+++ src/helpers/social-networks.ts
@@ -0,0 +1,14 @@
+export const SOCIAL_NETWORKS = [
+  {
+    icon: 'github',
+    link: 'https://github.com/kkulebaev',
+  },
+  {
+    icon: 'twitter',
+    link: 'https://twitter.com/kkulebaev',
+  },
+  {
+    icon: 'telegram',
+    link: 'https://t.me/kulebaev_official',
+  },
+] as const
