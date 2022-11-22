aefc90211f52288320adb5dcf3add9bb0ffc7c4d
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-23 00:52:59 +0300

    chore: add page-title component

---
+++ src/components/page-title.vue
@@ -0,0 +1,31 @@
+<template>
+  <h1 class="page-title">{{ title }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageTitle',
+
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    icon: {
+      type: String,
+      default: null,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.page-title {
+  font-size: 3.6rem;
+  font-weight: 700;
+  color: $black;
+}
+</style>
