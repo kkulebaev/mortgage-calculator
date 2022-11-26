8d1ab37869a69894149e6288c2f2abd70f9320f0
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-26 21:35:54 +0300

    chore: chaneg nav icon to fontawesome

--- src/helpers/nav.ts
+++ src/helpers/nav.ts
@@ -1,21 +1,17 @@
-import Home from '@/assets/img/home.svg'
-import Rows from '@/assets/img/rows.svg'
-import Faq from '@/assets/img/faq.svg'
-
 export const NAV_ITEMS = [
   {
     id: 'home',
     to: { name: 'MainPage' },
-    icon: Home,
+    icon: ['fas', 'house'],
   },
   {
     id: 'detail',
     to: { name: 'DetailPage' },
-    icon: Rows,
+    icon: ['fas', 'table'],
   },
   {
     id: 'faq',
     to: { name: 'FaqPage' },
-    icon: Faq,
+    icon: ['fas', 'circle-question'],
   },
 ] as const
