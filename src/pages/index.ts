2ea6551ae6597d997a119ec9685c05833c3dc5f5
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-27 21:31:01 +0300

    config: 'simple-import-sort/exports': 'error'

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as MainPage } from './main-page.vue'
 export { default as DetailPage } from './detail-page.vue'
-export { default as NotFoundPage } from './not-found-page.vue'
 export { default as FAQPage } from './faq-page.vue'
+export { default as MainPage } from './main-page.vue'
+export { default as NotFoundPage } from './not-found-page.vue'
