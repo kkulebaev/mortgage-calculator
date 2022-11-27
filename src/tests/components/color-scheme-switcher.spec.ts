1037ea3348f3dfd8183590f4f096d9d7d42976fe
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-27 18:32:03 +0300

    test: add some test for color-scheme-switcher

---
+++ src/tests/components/color-scheme-switcher.spec.ts
@@ -0,0 +1,35 @@
+import { mount, type VueWrapper } from '@vue/test-utils'
+import { it, describe, expect } from 'vitest'
+import { ColorSchemeSwitcher } from '@/components'
+
+describe('ColorSchemeSwitcher', () => {
+  let wrapper: VueWrapper<any>
+
+  const createComponent = () => {
+    wrapper = mount(ColorSchemeSwitcher)
+  }
+
+  it('should after mounted set to html class "light"', async () => {
+    createComponent()
+
+    expect(document.documentElement.outerHTML).toContain('light')
+  })
+
+  it('should set to html class "dark" when switcher has dark value', async () => {
+    createComponent()
+    const inputDark = wrapper.find('input[value="dark"]')
+
+    await inputDark.setValue(true)
+
+    expect(document.documentElement.outerHTML).toContain('dark')
+  })
+
+  it('should set to html class "light" when switcher has light value', async () => {
+    createComponent()
+    const inputLight = wrapper.find('input[value="light"]')
+
+    await inputLight.setValue(true)
+
+    expect(document.documentElement.outerHTML).toContain('light')
+  })
+})
