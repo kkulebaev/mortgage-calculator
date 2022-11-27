4b9f5fe82ec560fed53b8a94c261ddd59d16ae0e
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-27 15:56:25 +0300

    chore: move default values of form to helpers

--- src/store/main.ts
+++ src/store/main.ts
@@ -1,49 +1,12 @@
 import { defineStore } from 'pinia'
-import { PERIOD, MORTGAGE_TYPE } from '@/helpers'
 import { calcMortgage } from '@/services'
-
-export interface PaymentByMonth {
-  id: number
-  monthPay: number
-  repayPer: number
-  repayBody: number
-  debtEnd: number
-}
-
-export interface Input {
-  amount: number
-  term: number
-  period: PERIOD
-  rate: number
-  type: MORTGAGE_TYPE
-}
-
-export interface Output {
-  takeValue: number
-  repayValue: number
-  overpaymentValue: number
-  paymentTable: PaymentByMonth[]
-}
+import { DEFAULT_INPUT, DEFAULT_OUTPUT } from '@/helpers'
+import type { Input, Output } from '@/helpers'
 
 interface State {
   inputValues: Input
   outputValues: Output
 }
-
-export const DEFAULT_INPUT = (): Input => ({
-  amount: 1000000,
-  term: 12,
-  period: PERIOD.months,
-  rate: 10,
-  type: MORTGAGE_TYPE.an,
-})
-
-const DEFAULT_OUTPUT = (): Output => ({
-  takeValue: 0,
-  repayValue: 0,
-  overpaymentValue: 0,
-  paymentTable: [],
-})
 
 export const useMainStore = defineStore('main', {
   state: (): State => ({
