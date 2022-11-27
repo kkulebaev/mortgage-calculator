1eb1332a48771ba3e986a0fb3387dfcd4ffc0019
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-28 00:45:41 +0300

    fix: import sort

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-
+import { defineConfig } from 'vite'
 import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
