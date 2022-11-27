eb624a12977c291cfb7a6ac95bac6f7e7eb07889
kkulebaev <konstantinkulebaev@gmail.com>
2022-11-27 21:31:01 +0300

    config: 'simple-import-sort/exports': 'error'

--- .eslintrc.js
+++ .eslintrc.js
@@ -68,6 +68,7 @@ module.exports = {
       },
     ],
     'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
   },
   overrides: [
     {
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,15 +1,15 @@
-export { NAV_ITEMS } from './nav'
+export { COLOR_SCHEME } from './color-scheme'
+export type { Input, Output, PaymentByMonth } from './default-input-output'
+export { DEFAULT_INPUT, DEFAULT_OUTPUT } from './default-input-output'
+export { DETAIL_FIELDS } from './detail'
+export { FAQ } from './faq'
 export {
-  TYPE_MORTGAGE_OPTIONS,
-  PERIOD_OPTIONS,
   MORTGAGE_TYPE,
   MORTGAGE_TYPE_LABEL,
   PERIOD,
   PERIOD_LABEL,
+  PERIOD_OPTIONS,
+  TYPE_MORTGAGE_OPTIONS,
 } from './form'
-export { DETAIL_FIELDS } from './detail'
+export { NAV_ITEMS } from './nav'
 export { SOCIAL_NETWORKS } from './social-networks'
-export { FAQ } from './faq'
-export { COLOR_SCHEME } from './color-scheme'
-export { DEFAULT_INPUT, DEFAULT_OUTPUT } from './default-input-output'
-export type { Input, Output, PaymentByMonth } from './default-input-output'
