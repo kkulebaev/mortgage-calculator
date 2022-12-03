8d138db5100db0905738ae17b17f97bf3a13e8ee
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-03 14:23:36 +0300

    chore: refactoring app form

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
 export { default as numberWithSpaces } from './number-with-spaces'
 export { default as roundNumber } from './round-number'
-export { default as toNumber } from './to-number'
--- src/utils/to-number.ts
+++
@@ -1,3 +0,0 @@
-export default function toNumber(val: string) {
-  return Number(val.replace(/[^\d]/g, ''))
-}
