c68e52759b377b80f4fb7a649de3925e5c10aa68
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-21 18:47:54 +0300

    change BASE_URL backend

--- src/api/api-instance.ts
+++ src/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_APP_URL + import.meta.env.VITE_API_PORT,
+  baseURL: import.meta.env.VITE_BASE_URL,
 })
 
 export { apiInstance }
