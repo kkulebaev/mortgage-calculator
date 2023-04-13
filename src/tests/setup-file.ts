c98ea0b07f7c72fbdc01e0e9f7b6a7d2823c7c7c
kkulebaev <konstantinkulebaev@gmail.com>
2023-04-13 15:01:08 +0300

    test: add i18n to setup test

--- src/tests/setup-file.ts
+++ src/tests/setup-file.ts
@@ -1,4 +1,8 @@
-import { enableAutoUnmount } from '@vue/test-utils'
+import { config, enableAutoUnmount } from '@vue/test-utils'
 import { afterEach } from 'vitest'
 
+import { i18n } from '@/plugins/i18n'
+
 enableAutoUnmount(afterEach)
+
+config.global.plugins = [i18n]
