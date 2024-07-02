3475317d07e596ea323251bd40d2ef8fa285fc6b
kkulebaev <konstantinkulebaev@gmail.com>
2024-07-02 17:45:46 +0400

    fix: add BackendFetch integration

--- src/plugins/tolgee.ts
+++ src/plugins/tolgee.ts
@@ -1,4 +1,11 @@
-import { DevTools, FormatSimple, LanguageStorage, Tolgee, VueTolgee } from '@tolgee/vue'
+import {
+  BackendFetch,
+  DevTools,
+  FormatSimple,
+  LanguageStorage,
+  Tolgee,
+  VueTolgee,
+} from '@tolgee/vue'
 
 export const enum Language {
   EN = 'en',
@@ -11,6 +18,7 @@ const tolgee = Tolgee()
   .use(DevTools())
   .use(FormatSimple())
   .use(LanguageStorage())
+  .use(BackendFetch({ prefix: 'https://cdn.tolg.ee/585d9283cc54953be971b3997c848c00' }))
   .init({
     apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
     apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
