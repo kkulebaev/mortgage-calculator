e7ff86cf20cae8055dbd93cef8315da3be712a80
kkulebaev <konstantinkulebaev@gmail.com>
2024-07-02 14:12:54 +0400

    feat: add slavicLangPluralRule

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -18,8 +18,29 @@ export const i18n = createI18n<[MessageSchema], LANG, false>({
     [LANG.en]: EN,
     [LANG.ru]: RU,
   },
+  pluralRules: {
+    [LANG.ru]: slavicLangPluralRule,
+  },
 })
 
 function getUserLocale() {
   return localStorage.getItem('locale') ?? LANG.en
+}
+
+/* https://vue-i18n.intlify.dev/guide/essentials/pluralization.html#custom-pluralization */
+function slavicLangPluralRule(choice: number, choicesLength: number): 0 | 1 | 2 | 3 {
+  if (choice === 0) {
+    return 0
+  }
+
+  const teen = choice > 10 && choice < 20
+  const endsWithOne = choice % 10 === 1
+  if (!teen && endsWithOne) {
+    return 1
+  }
+  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+    return 2
+  }
+
+  return choicesLength < 4 ? 2 : 3
 }
